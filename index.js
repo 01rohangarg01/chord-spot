e790d97ca8777c34f2c0c94e64e60129d16e0d1e
Rohan Garg <01rohangarg01@gmail.com>
2022-06-22 22:24:54 +0530

    Removed comments and unused code

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-// document.getElementById("submit").onclick = function submit(){
-// let notes = []
-//     notes.push(document.getElementById("enternotes").value);
-//     notes = JSON.stringify(notes);
-//     console.log(notes);
-// }
-
-
-// document.getElementById("enternotes").onkeydown = function entur(e){
-//     if(e.key == "Enter"){
-//         console.log("helu I'm clicked")
-//         // submit();
-//     }
-// }
-
-const notes = {
-    "C or B sharp": "C",
-    "C sharp or D flat": "C#",
-    "E or F flat":"E",
-    "F or E sharp":"F",
-    "B or C flat":"B",
-    "D":"D",
-    "G":"G",
-    "A":"A",
-    "D sharp or E flat":"D#",
-    "F sharp or G flat":"F#",
-    "G sharp or A flat":"G#",
-    "A sharp or B flat":"A#"
-};
-
-
-
 let oparr = [];
 function toggleNote(a) {
     if (oparr.includes(a)) {
@@ -43,10 +11,10 @@ function toggleNote(a) {
     printchord()
 } 
 
-function printnotes(){
+function printnotes() {
      document.getElementById("printnotes").innerHTML = oparr.join(", ");
 }
-function printchord(){
+
+function printchord() {
     document.getElementById("printchord").innerHTML = Tonal.Chord.detect(oparr).join(", ");
 }
-
